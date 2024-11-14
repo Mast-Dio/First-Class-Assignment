e01283dd2b61a74c6c701f06f9f6f47d13779dcd
Mast-Dio <davididiake9@gmail.com>
2024-11-14 22:39:03 +0100

    Add files via upload

---
+++ MathUtils.java
@@ -0,0 +1,24 @@
+public class MathUtils {
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+    public static boolean isPalindrome(String str) {
+        if (str == null) {
+            return false;
+        }
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
